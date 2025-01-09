203797b0aeb9699cd578627be41d7aa1aa9f995d
lewshubei <bei-02@graduate.utm.my>
2025-01-09 15:08:30 +0800

    sql setting

--- UTMRegistEase/src/main/java/config/HibernateConfig.java
+++ UTMRegistEase/src/main/java/config/HibernateConfig.java
@@ -14,7 +14,7 @@ import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
-@ComponentScan(basePackages = {"com.example.service", "com.example.entity"})
+@ComponentScan(basePackages = {"com.example.DBservice", "com.example.entity"})
 public class HibernateConfig {
 
     @Bean
